c2db6e3fcb93ecc811dbbf1b92aaf496c82d7d95
lee <lshter01@campus.haifa.ac.il>
2018-03-18 15:03:52 +0200

    add all dayar screens

--- app/src/main/java/com/lee/minted/dayarim.java
+++ app/src/main/java/com/lee/minted/dayarim.java
@@ -1,7 +1,11 @@
 package com.lee.minted;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class dayarim extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class dayarim extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dayarim);
+
+        final ImageButton back= (ImageButton)findViewById(R.id.back_bu);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(dayarim.this,Menu_Dayar_Activity.class
+                );
+                startActivity(intent);
+            }
+        });
+
     }
 }
